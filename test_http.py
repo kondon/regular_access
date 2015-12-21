b51f88e50ac17720b27dd6f268e6869b5276a94c
Kondo <shinji_kondo@post.pioneer.co.jp>
2015-12-21 14:36:19 +0900

    add

---
+++ test_http.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import requests
+
+if __name__ == '__main__':
+    query = {
+        'id': 1
+    }
+    r = requests.get('https://test-sample-kondo.herokuapp.com/web/index.php', params=query)
+    print r.status_code
+    print r.encoding
+    print r.headers
+    print r.text
+    print r.json()
